29825ecf93e124f186dbb14501ba0bfea1e8a957
SoftDuct <84712530+SoftDuct@users.noreply.github.com>
2021-11-19 15:35:07 +0900

    Initial Commit

---
+++ Street RunnerTests/Street_RunnerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Street_RunnerTests.swift
+//  Street RunnerTests
+//
+//  Created by 木本朝陽 on 2021/11/19.
+//
+
+import XCTest
+@testable import Street_Runner
+
+class Street_RunnerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Street RunnerUITests/Street_RunnerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Street_RunnerUITestsLaunchTests.swift
+//  Street RunnerUITests
+//
+//  Created by 木本朝陽 on 2021/11/19.
+//
+
+import XCTest
+
+class Street_RunnerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
